295088f4afcb94c2d958534f6a750aebbb12ad4e
mduy2007 <minhduy.contactwithme@gmail.com>
2026-09-22 08:05:24 +0700

    complete task 3 | add dorm, floor, room in json | bugs: not

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from service.student_service import students_service
+# from service.student_service import students_service
+from service.admin_service import info_student
 
 if __name__ == "__main__":
-    info = students_service(file_path="data/info_student.json")
-    data = info.add_info_student()
+    # info = students_service(file_path="data/info_student.json")
+    # data = info.add_info_student()
     # print("Test quá trình lưu pass")
     # print(save_pw())
-    print("test thêm sinh viên")
-    print(data)
+    # print("test thêm sinh viên")
+    # print(data)
+
+    data = info_student(file_path="data/dorm.json")
+    nhapktx = data.add_dorm()
+    print("test thêm ktx")
+    print(nhapktx)
--- service/admin_service.py
+++ service/admin_service.py
@@ -21,15 +21,30 @@ class info_student(file_works):
 
     #hàm thêm kí túc xá
     def add_dorm(self):
-        self.ki_tuc_xa = inquirer.text (
-            message="Vui Lòng Nhập Số Toà: ",
+        #Thêm Toà Ktx
+        self.ki_tuc_xa = inquirer.number (
+            message="Vui Lòng Nhập Toà: ".strip(),
             mandatory=True,
-            mandatory_message="Trường này buộc nhập"
+            float_allowed=False,
+            mandatory_message="Trường này buộc nhập",
+            validate=lambda val: val is not None or "Số Toà Không Hợp Lệ!"
         ).execute()
+        self.dorm_phase1 = ["G", self.ki_tuc_xa]
+        self.dorm_phase2 = "".join(map(str,self.dorm_phase1))
+
+        #thêm số tầng
+        self.floor = inquirer.number (
+            message="Vui Lòng Nhập Số Tầng: ".strip(),
+            mandatory=True,
+            float_allowed=False,
+            mandatory_message="Trường này là bắt buộc!",
+            validate=lambda val: val is not None or "Số Tầng Không Hợp Lệ!"
+        ).execute()
+
         #tự động sinh thêm số phòng theo tầng đã nhập
         self.so_phong = []
-        self.so_toa = int(self.ki_tuc_xa)
-        for i in range(self.so_toa):
+        self.so_tang = int(self.floor)
+        for i in range(self.so_tang):
             for j in range(12):
                 if j < 9:
                     self.ghep = [i+1,j+1]
@@ -39,17 +54,20 @@ class info_student(file_works):
                     self.ghep = [i+1,j+1]
                     self.nhap = "".join(map(str, self.ghep))
                     self.so_phong.append(self.nhap)
-        #tạo vòng lặp thêm vào dict
-        """
-        Định dạng vòng lặp:
-        [
-            G1
-            [
-                101
-                ...
-                112
-            ]
-            ...
-        ]
-        """
-
+        #xây dựng vòng lặp thêm dữ liệu vào file json
+        u = 0
+        for i in range(self.so_tang):
+            self.ket_noi = ["Tầng",i+1]
+            self.nhap_lai = " ".join(map(str, self.ket_noi))
+            for j in range(u, u + 12):
+                self.data.append(
+                    {
+                        "id_dorm": self.dorm_phase2,
+                        "id_floor": self.nhap_lai,
+                        "id_room": self.so_phong[j]
+                    
+                    }
+                )
+            u += 12
+        self.save_file(self.data)
+        return self.data     
